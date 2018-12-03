deb41adb12cf17b69937110f562c2f4b8a5be974
Bruno Pazos <pazosbruno@gmail.com>
2018-12-03 13:53:42 -0300

    koch snowflake

--- koch.py
+++ koch.py
@@ -8,26 +8,29 @@ import os
 
 WIDTH=640
 HEIGHT=480
-GEN = 5 
+DELAY_MS = 500
+GEN = 8 
 ANGLE = 60
-LENGTH_LINE = WIDTH//0.6
+LENGTH_LINE = WIDTH/0.6
 FACTOR = 3
 AXIOM = 'F'
 START_RULE = 'F--F--F'
+#START_RULE = 'F'
 RULE = 'F+F--F+F'
 
+black = (0, 0, 0, 255)
 white = (255, 255, 255, 255)
 red = (255, 0, 0, 255)
 green = (0, 255, 0, 255)
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-
-offset_x = LENGTH_LINE/6
-offset_y = LENGTH_LINE/10
+fs_in_rule = len([f for f in RULE if f == 'F'])
+offset_x = LENGTH_LINE/(fs_in_rule+FACTOR-1)
+offset_y = LENGTH_LINE/(ANGLE/(fs_in_rule+FACTOR-1))
 
 cursor = {'x': WIDTH/2 - offset_x, 
           'y': HEIGHT/2 - offset_y, 
-          'angle': 0}
+          'angle': 0, 
+          'factor': FACTOR}
 
 def generator(rule, gen=1):
     #Recursive function to replace the original axiom with rule (which changes over generations).
@@ -53,6 +56,7 @@ def update_cursor(screen, color, cursor, rule, l):
 
 def main_loop():
     #Loops "infinitely" starting from axiom and applying rules until gen==GEN.
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     running = 1
 
     while running:
@@ -60,16 +64,16 @@ def main_loop():
         if event.type == pygame.QUIT:
             running = 0
         if event.type == pygame.VIDEORESIZE:
-            surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
         l = LENGTH_LINE
         for gen in range(GEN):
-            screen.fill([0, 0, 0])
+            screen.fill(black)
             #pygame.draw.line(screen, red, (0, HEIGHT/2), (WIDTH, HEIGHT/2)) # half screen line over X axis
             #pygame.draw.line(screen, green, (WIDTH/2, 0), (WIDTH/2, HEIGHT)) # half screen line over Y axis
-            pygame.time.delay(500) #Delay to be able to see differences between generations 
+            pygame.time.delay(DELAY_MS) #Delay to be able to see differences between generations 
             rule = generator(START_RULE, gen)
-            l /= FACTOR 
+            l /= cursor['factor'] 
             update_cursor(screen, white, cursor, rule, l)
             pygame.display.update()    
         
